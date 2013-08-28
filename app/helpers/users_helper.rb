004993dc0c69096679d15194b22016478007cc3c
abdulmatinshah <abdulmateenshah@gmail.com>
2013-08-27 20:08:51 -0700

    improve application views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def gravatar_for(user, size = 50 )
-    hashed_email = Digest::MD5.hexdigest(user.email).strip.downcase
+    hashed_email = Digest::MD5.hexdigest(user.email.strip.downcase)
     gravatar_url = "http://www.gravatar.com/avatar/#{hashed_email}.jpg?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
